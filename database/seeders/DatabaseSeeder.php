008d5297f617615f3f6604a7336d58affe1e74ab
Mubashir Khan <codephantom404@gmail.com>
2025-10-05 02:13:13 +0500

    spatie role permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,24 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'seller']);
+        Role::create(['name' => 'buyer']);
+
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            'name' => 'Admin User',
+            'email' => 'admin@system.com',
+
+        ])->assignRole('admin');
+
+        User::factory()->create([
+            'name' => 'Seller User',
+            'email' => 'seller@system.com',
+        ])->assignRole('seller');
+
+        User::factory()->create([
+            'name' => 'Buyer User',
+            'email' => 'buyer@system.com',
+        ])->assignRole('buyer');
     }
 }
